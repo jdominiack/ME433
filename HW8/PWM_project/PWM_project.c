6fa562e79ddb6d29434bc008a83739647dffc6d3
Josh Dominiack <jdominiack12835@gmail.com>
2025-05-18 03:11:02 -0500

    HW8 Draft

---
+++ HW8/PWM_project/PWM_project.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/pwm.h"
+
+
+#define SERVO_PIN 21 // the built in LED on the Pico
+#define WRAP 30000
+
+void PWM_init();
+void set_angle(int angle);
+
+
+int main()
+{
+    stdio_init_all();
+
+    gpio_set_function(SERVO_PIN, GPIO_FUNC_PWM); // Set the LED Pin to be PWM
+
+    PWM_init();
+
+    int servoAngle = 0;
+    bool incline = true;
+
+
+    while (true) {
+        set_angle(120);
+
+        sleep_ms(1000);
+
+        set_angle(180);
+
+        sleep_ms(1000);
+
+        // if (incline){
+        //     servoAngle += 10;
+        //     if (servoAngle > 180){
+        //         incline = false;
+        //         servoAngle -= 20;
+        //     }
+        // }
+        // else {
+        //     servoAngle -= 10;
+        //     if (servoAngle < 0){
+        //         incline = true;
+        //         servoAngle += 20;
+        //     }
+        // }  
+
+
+        
+        
+    }
+}
+
+void PWM_init(){
+    uint slice_num = pwm_gpio_to_slice_num(SERVO_PIN); // Get PWM slice number
+    float div = 100; // must be between 1-255
+    pwm_set_clkdiv(slice_num, div); // divider
+    //uint16_t wrap = 1000; // when to rollover, must be less than 65535
+    pwm_set_wrap(slice_num, (uint16_t)WRAP);
+    pwm_set_enabled(slice_num, true); // turn on the PWM
+    pwm_set_gpio_level(SERVO_PIN, (uint16_t)WRAP * 0.025); // set the duty cycle to 50%
+}
+
+void set_angle(int angle){
+    if (angle <= 180 & angle >= 0){
+        float duty_cycle = ((angle/180) * (0.125 - 0.025)) + 0.025;
+
+        uint16_t wrap = 1000;
+        pwm_set_gpio_level(SERVO_PIN, (uint16_t)(WRAP * duty_cycle));
+    }
+
+}
