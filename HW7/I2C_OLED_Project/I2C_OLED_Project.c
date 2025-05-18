adac10c9c46326b27279cde3bd2492392e5f31e6
Josh Dominiack <jdominiack12835@gmail.com>
2025-05-18 01:34:04 -0500

    HW7 Final

--- HW7/I2C_OLED_Project/I2C_OLED_Project.c
+++ HW7/I2C_OLED_Project/I2C_OLED_Project.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/adc.h"
 #include "hardware/i2c.h"
+#include "hardware/gpio.h"
+
 #include "ssd1306.h"
+#include "font.h"
 
 // I2C defines
 // This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
@@ -12,11 +16,18 @@
 
 int pico_led_init(void);
 void pico_set_led(bool led_on);
+void drawChar(int xO, int yO, char character);
+void drawString(int xO, int yO, char str[]);
 
 int main()
 {
     stdio_init_all();
-    //ssd1306_setup();
+
+    adc_init();
+    adc_gpio_init(26);
+    adc_select_input(0);
+
+    const float conversion_factor = 3.3f / (1 << 12);
 
     // I2C Initialisation. Using it at 400Khz.
     i2c_init(I2C_PORT, 400*1000);
@@ -25,25 +36,47 @@ int main()
     gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
     gpio_pull_up(I2C_SDA);
     gpio_pull_up(I2C_SCL);
+
+    pico_led_init();
+    pico_set_led(true);
+
+    ssd1306_setup();
+
     // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
 
     bool pixelOn = false;
 
+    //Increase x -> pixel moves left
+    //Increase y -> pixel moves up
+
+    int frameCounter = 0;
+    float fps = 0;
+    uint64_t start = to_us_since_boot(get_absolute_time());
+    uint64_t stop;
+
     while (true) {
+        sleep_ms(1);
 
-        sleep_ms(1000);
+        uint16_t adcVolt = adc_read();
+        char vReport[50];
+        sprintf(vReport, "Voltage: %.2fV", adcVolt*conversion_factor);
 
-        if (pixelOn){
-            ssd1306_drawPixel(64, 16 , 0);
-            pixelOn = false;
+        char fpsReport[50];
+        sprintf(fpsReport, "FPS: %.2f", fps);
+
+        drawString(120,24,vReport);
+        drawString(65, 0, fpsReport);
+
+        ssd1306_update();
+        frameCounter += 1;
+
+        if (frameCounter%10 == 0){
+            stop = to_us_since_boot(get_absolute_time());
+            fps = (float)(frameCounter/((float)(stop-start)/1000000));
+
+            start = to_us_since_boot(get_absolute_time());
+            frameCounter = 0;
         }
-        else {
-            ssd1306_drawPixel(64, 16 , 1);
-            pixelOn = true;
-        }
-
-        ssd1306_update();       
-
 
         if (gpio_get(PICO_DEFAULT_LED_PIN)){
             pico_set_led(false);
@@ -76,5 +109,32 @@ void pico_set_led(bool led_on) {
     // Ask the wifi "driver" to set the GPIO on or off
     cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, led_on);
 #endif
+}
+
+void drawChar(int xO, int yO, char character){
+    char col;
+    for (int i = 0; i < 5; i++){
+        col = ASCII[(int)character-32][i];
+        for (int j = 0; j < 8; j++){
+            if ((col >> j) & 0b1){
+                ssd1306_drawPixel(xO-i,yO+7-j, 1);
+            }
+            else{
+                ssd1306_drawPixel(xO-i,yO+7-j, 0);
+            }
+        }
+    }
+}
+
+void drawString(int xO, int yO, char str[]){
+    int y = yO;
+    int x = xO;
+    for (int i = 0; str[i] != '\0'; i++){
+        if ((x-7*i - 5) < 0){
+            y -= 8;
+            x = 120;
+        }
+        drawChar(x-7*i, y, str[i]);
+    }
 }
 
