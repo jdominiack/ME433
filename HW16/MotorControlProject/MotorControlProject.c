ef3e38ee990424be5e3a62db9e1341bb0528cd84
Josh Dominiack <jdominiack12835@gmail.com>
2025-06-02 22:29:23 -0500

    HW16 Final

--- HW16/MotorControlProject/MotorControlProject.c
+++ HW16/MotorControlProject/MotorControlProject.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "pico/stdlib.h"
+#include "hardware/pwm.h"
 
+#define PHASE_PIN 16
+#define ENABLE_PIN 17
 
-#define PHASE_PIN 2
-#define ENABLE_PIN 3
-
-#define FREQ 20 //kHz
+#define FREQ 75 //kHz
 
 int dutyCycle = 0;
 int cycleTime = (float)(1/(FREQ*1000)) * 1000000; //us
@@ -19,8 +19,17 @@ int main()
     gpio_init(PHASE_PIN);
     gpio_set_dir(PHASE_PIN, GPIO_OUT);
 
-    gpio_init(ENABLE_PIN);
-    gpio_set_dir(ENABLE_PIN, GPIO_OUT);
+    //gpio_init(ENABLE_PIN);
+    gpio_set_function(ENABLE_PIN, GPIO_FUNC_PWM);
+
+    uint slice_num = pwm_gpio_to_slice_num(ENABLE_PIN); // Get PWM slice number
+    float div = 2; // must be between 1-255
+    pwm_set_clkdiv(slice_num, div); // divider
+    uint16_t wrap = 1000; // when to rollover, must be less than 65535
+    pwm_set_wrap(slice_num, wrap);
+    pwm_set_enabled(slice_num, true); // turn on the PWM
+
+    pwm_set_gpio_level(ENABLE_PIN, 0); // set the duty cycle to 0%
 
     while (true) {
         int input = getchar_timeout_us(0);
@@ -28,30 +37,21 @@ int main()
             if((char)input == '+' && dutyCycle < 100){
                 //Increase duty cycle
                 dutyCycle += 1;
-                printf("Duty Cycle: %d", dutyCycle);
+                printf("Duty Cycle: %d\n\r", dutyCycle);
             }
             else if ((char)input == '-' && dutyCycle > -100){
                 //Decrease duty cycle
                 dutyCycle -= 1;
-                printf("Duty Cycle: %d", dutyCycle);
-            }           
+                printf("Duty Cycle: %d\n\r", dutyCycle);
+            }
+            pwm_set_gpio_level(ENABLE_PIN, wrap*abs(dutyCycle)/100);           
         }
-
         if (dutyCycle > 0){
-            gpio_put(ENABLE_PIN, 1);
             gpio_put(PHASE_PIN, 0);
         }
         else {
-            gpio_put(ENABLE_PIN, 1);
             gpio_put(PHASE_PIN, 1);
         }
-
-        sleep_us((int)(dutyCycle*cycleTime/100));
-
-        gpio_put(ENABLE_PIN, 0);
-        gpio_put(PHASE_PIN, 0);
-
-        sleep_us((int)((100-dutyCycle)*cycleTime/100));
        
     }
 }
